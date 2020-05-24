7a35d6e16d95d824b6b0120467d1a80bb15282e6
nitinsharmacs <ramujha266@gmail.com>
2020-05-24 07:55:13 +0530

    updated database file

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 let _db;
-const DB_URL = process.env.PORT || 'mongodb://127.0.0.1:27017';
+const DB_URL = process.env.DB_URL || 'mongodb://127.0.0.1:27017';
 const dbConnection = cb => {
 	MongoClient.connect(DB_URL, {useNewUrlParser:true}).then(client=>{
 		_db = client.db('gogaga');
